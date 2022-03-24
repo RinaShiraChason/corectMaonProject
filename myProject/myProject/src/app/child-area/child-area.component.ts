b34548aacb41c870c2bf831953ef8ff21c554d92
Rina Shira <rinashira11@gmail.com>
2022-03-24 10:31:23 +0200

    Shira

--- myProject/myProject/src/app/child-area/child-area.component.ts
+++ myProject/myProject/src/app/child-area/child-area.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { ActivityUpdate } from '../classes/ActivityUpdate';
 import { DayCare } from '../classes/DayCare';
 import { DayCareService } from '../services/day-care.service';
+import {ActivityUpdateService} from '../services/activity-update.service';
+
 
 @Component({
   selector: 'app-child-area',
@@ -9,18 +12,70 @@ import { DayCareService } from '../services/day-care.service';
 })
 export class ChildAreaComponent implements OnInit {
   dayCareByKids: DayCare;
+  activityUpdateByClass: ActivityUpdate;
   today = new Date();
 
-  constructor(private dcSerice: DayCareService) { }
+  constructor(private dcSerice: DayCareService, private auSerice: ActivityUpdateService,private renderer: Renderer2) { }
 
+  cards = [
+    {img: 'assets/images/g(16).jpg'},
+    {img: 'assets/images/g(17).jpg'},
+    {img: 'assets/images/g(18).jpg'},
+    {img: 'assets/images/g(19).jpg'},
+    {img: 'assets/images/g(20).jpg'},
+    {img: 'assets/images/g(21).jpg'},
+    {img: 'assets/images/g(28).jpg'},
+    {img: 'assets/images/g(30).jpg'},
+    {img: 'assets/images/g(31).jpg'},
+    {img: 'assets/images/g(32).jpg'},
+    {img: 'assets/images/g(23).jpg'},
+    {img: 'assets/images/g(24).jpg'},
+    {img: 'assets/images/g(25).jpg'},
+    {img: 'assets/images/g(26).jpg'},
+    {img: 'assets/images/g(27).jpg'},
+  ];
+ 
+  slides: any = [[]];
+  
+  imgDrive(){
+  let url = 'https//drive.google.com/drive/folders/1Hm9ZSihyrqaGPJLAwOClsPSInJlzAeZY?usp=sharing';
+  window.open(url);
+  }
+  
+  chunk(arr: any, chunkSize: number) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }
+
+  ngAfterViewInit() {
+    const buttons = document.querySelectorAll('.btn-floating');
+    buttons.forEach((el: any) => {
+      this.renderer.removeClass(el, 'btn-floating');
+      this.renderer.addClass(el, 'px-3');
+      this.renderer.addClass(el.firstElementChild, 'fa-3x');
+    });
+  }
+  
   ngOnInit(): void {
     this.getAll();
+    this.getActivityUpdateByClass();
+    this.slides = this.chunk(this.cards, 3);
   }
+  
   getAll() {
     this.dcSerice.getDayCareByKids(3).subscribe((x) => {
       this.dayCareByKids = x;
     });
 
+  }
+
+  getActivityUpdateByClass() {
+    this.auSerice.getTodayActivityUpdateByClass(1).subscribe((x) => {
+      this.activityUpdateByClass = x;
+    });
   }
 
 }
